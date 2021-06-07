5f0571094434064c6c75948af5516f2282d8f60e
winston1214 <winston1214@naver.com>
2021-06-07 16:56:52 +0900

    UI code

--- crawlingui.py
+++ crawlingui.py
@@ -2,8 +2,10 @@ import sys
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
 from naver_google_crawling import naver,google
-from digital_paper import digital_news,newspim
+from digital_paper import digital_news,newspim,AItimes
 import pandas as pd
+import tkinter as tk
+from tkinter import messagebox
 # form_class = uic.loadUiType(r"D:\crawling\crawling.ui")[0]
 form_class = uic.loadUiType("crawling.ui")[0]
 class WindowClass(QMainWindow, form_class) :
@@ -15,13 +17,9 @@ class WindowClass(QMainWindow, form_class) :
         self.naver_check = 0
         self.google_check = 0
         self.keyword = ''''''
-        self.op = ''
         self.other = 0
         self.checkBox.clicked.connect(self.check_naver)
         self.checkBox2.clicked.connect(self.check_google)
-        self.radioButton3.clicked.connect(self.AND_check)
-        self.radioButton4.clicked.connect(self.OR_check)
-        self.radioButton5.clicked.connect(self.None_check)
         self.save_button.clicked.connect(self.other_information)
         self.Search.clicked.connect(self.crawling)
 
@@ -37,33 +35,40 @@ class WindowClass(QMainWindow, form_class) :
         else:
             self.google_check = 0
         print('google check!')
-    def AND_check(self):
-        self.op = 'AND'
-    def None_check(self):
-        self.op = 'None'
-    def OR_check(self):
-        self.op = 'OR'
     def other_information(self):
         self.other = 1
     def crawling(self):
         self.keyword = self.textEdit.toPlainText()
         df = pd.DataFrame()
         if self.naver_check:
-            naver_df = naver(self.keyword,self.op)
-            df = pd.concat([df,naver_df])
+            naver_df = naver(self.keyword)
+            if len(naver_df):
+                df = pd.concat([df,naver_df])
         elif self.google_check:
-            google_df = google(self.keyword,self.op)
-            df = pd.concat([df,google_df])
+            google_df = google(self.keyword)
+            if len(google_df):
+                df = pd.concat([df,google_df])
         elif self.naver_check and self.google_check:
-            naver_df = naver(self.keyword,self.op)
-            google_df = google(self.keyword,self.op)
-            df = pd.concat([df,naver_df,google_df])
+            naver_df = naver(self.keyword)
+            google_df = google(self.keyword)
+            if len(naver_df) or len(google_df):
+                df = pd.concat([df,naver_df,google_df])
         if self.other:
             digital_df = digital_news()
             digital_df2 = newspim()
-            df=pd.concat([df,digital_df,digital_df2])
-        df.to_excel('merge_data.xlsx',index=False)
-
+            digital_df3 = AItimes()
+            df=pd.concat([df,digital_df,digital_df2,digital_df3])
+        if len(df)>0:
+            df.to_excel('merge_data.xlsx',index=False)
+            root2 = tk.Tk()
+            msg = messagebox.showinfo(title='Complete',message='Complete!')
+            if msg == 'ok':
+                root2.destroy()
+        else:
+            root2 = tk.Tk()
+            msg = messagebox.showerror(title='Nothing',message='Nothing')
+            if msg == 'ok':
+                root2.destroy()
 if __name__ == "__main__" :
     app = QApplication(sys.argv)
     myWindow = WindowClass()
